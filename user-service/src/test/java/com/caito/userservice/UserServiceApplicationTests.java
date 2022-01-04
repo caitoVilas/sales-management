b3d61c151fe41f7d906aa28e5e8ad48e95e0a226
caito <caitocd@gmail.com>
2022-01-04 11:40:32 -0300

    added user service

---
+++ user-service/src/test/java/com/caito/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caito.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
